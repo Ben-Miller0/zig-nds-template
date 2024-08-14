71bc919fef813aa7bf8bc3d0ce6db4fbf010dc64
Ben-Miller0 <ben.miller@antimo.com>
2024-08-14 15:27:21 +0100

    frame and press tracking

--- src/arm9/arm9.zig
+++ src/arm9/arm9.zig
@@ -4,9 +4,11 @@ export fn _start() noreturn {
     while (true) {}
 }
 
-const POWCNT1: *u32 = @ptrFromInt(0x04000304);
 const DISPCNT: *u32 = @ptrFromInt(0x04000000);
+const VCOUNT: *u16 = @ptrFromInt(0x04000006);
+const KEYINPUT: *u16 = @ptrFromInt(0x04000130);
 const VRAMCNT_A: *u8 = @ptrFromInt(0x04000240);
+const POWCNT1: *u32 = @ptrFromInt(0x04000304);
 const VRAM_A: [*]u16 = @ptrFromInt(0x06800000);
 
 fn main() void {
@@ -18,6 +20,74 @@ fn main() void {
 
     clear_screen(0b111110000000000);
     print_string("Hello, World!\x01");
+
+    var f_count: u8 = 0;
+    var a_count: u8 = 0;
+    var a_pressed = false;
+    while (true) : (f_count +%= 1) {
+        cursor_x = 0;
+        cursor_y = 1;
+        print_string("A:");
+        if (!a_pressed) {
+            if (KEYINPUT.* & 1 == 0) {
+                a_count +%= 1;
+                a_pressed = true;
+            }
+        } else {
+            if (KEYINPUT.* & 1 == 1) {
+                a_pressed = false;
+            }
+        }
+        print_byte(a_count);
+        cursor_x = 0;
+        cursor_y = 2;
+        print_string("F:");
+        print_byte(f_count);
+
+        while (VCOUNT.* < 192) {}
+        while (VCOUNT.* != 0) {}
+    }
+}
+
+pub fn print_byte(byte: u8) void {
+    print_char(switch (byte >> 4) {
+        0x0 => '0',
+        0x1 => '1',
+        0x2 => '2',
+        0x3 => '3',
+        0x4 => '4',
+        0x5 => '5',
+        0x6 => '6',
+        0x7 => '7',
+        0x8 => '8',
+        0x9 => '9',
+        0xA => 'A',
+        0xB => 'B',
+        0xC => 'C',
+        0xD => 'D',
+        0xE => 'E',
+        0xF => 'F',
+        else => '\x00',
+    });
+    print_char(switch (byte & 0x0F) {
+        0x0 => '0',
+        0x1 => '1',
+        0x2 => '2',
+        0x3 => '3',
+        0x4 => '4',
+        0x5 => '5',
+        0x6 => '6',
+        0x7 => '7',
+        0x8 => '8',
+        0x9 => '9',
+        0xA => 'A',
+        0xB => 'B',
+        0xC => 'C',
+        0xD => 'D',
+        0xE => 'E',
+        0xF => 'F',
+        else => '\x00',
+    });
 }
 
 pub fn print_string(string: []const u8) void {
